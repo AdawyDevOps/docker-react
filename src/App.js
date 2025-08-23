777c775795d615f7e5d781337b11fcf2401120d8
AdawyDevOps <ahmed.adawy.devops@gmail.coms>
2025-08-23 19:17:25 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          B7bk ya mama enty w baba w nono 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          صباحكم لبن حليب 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
